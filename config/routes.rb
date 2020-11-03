5792aa6c396702dd57345126f656a36c33b97fd5
awa <a87th_cm-prg@yahoo.co.jp>
2020-11-03 13:39:24 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
     
+  
     root to: 'tasks#index'
-    resources :tasks
+    resources :tasks    
+    
+    
+    get 'signup', to: 'users#new'
+    resources :users, only: [:create]
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
